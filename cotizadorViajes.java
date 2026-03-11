295b303567052f1e8536907aa4b7b2c7a1a7b5b4
Alan-Orlando <L25290571@cdguzman.tecnm.mx>
2026-03-11 14:14:26 +0000

    Avanze del examen Alan Orlando

--- cotizadorViajes.java
+++ cotizadorViajes.java
@@ -1,1 +1,41 @@
+/*Cantidad de adultos
+👦 Cantidad de niños
+🌙 Días de estancia (noches de hotel)
+🎫 Costo estándar del vuelo (tarifa base por adulto)
+🏨 Costo estándar de hospedaje (tarifa base por noche por adulto) */
 
+import Java.util.Scanner;
+
+    public class cotizadorViajes {
+
+
+    public static void main(string[] args ){
+
+    Scaner scan = new Scanner(System.in);
+    
+    
+    System.out.println("Cantidad de Adultos:");
+    cAdultos = scan.nextInt;
+
+    System.out.println("Cantidad de Niños:");
+    cNiños =scan.nextInt; 
+
+    System.out.println("Noches en el Hotel:");
+    nHotel =scan.nextInt;
+
+    System.out.println("Costo Estandar del Vuelo:");
+    cEstandarVuelo =scan.nextInt;
+
+    System.out.println("Costo Estandar del Hospedaje:");
+    cEstandarHospedaje =scan.nextInt;
+
+    
+
+
+
+
+    
+
+        
+    }
+}
--- herramientasCalculos.java
+++ herramientasCalculos.java
@@ -1,1 +1,27 @@
+public class herramientasCalculos {
 
+
+    public static int CostoVuelo (int cNiños,int cAdultos, int cEstandarVuelo ){
+    int cN,cA;
+    dNiños = cAdultos / 2;
+    cN = dNiños * cNiños;
+    cA= cAdultos * cEstandarVuelo;
+    tViaje=cN + cA;
+    return tViaje; 
+    }
+
+    public static int CostoHospedaje (int cNiños,int cAdultos, int cEstandarHospedaje ){
+    int cN,cA;
+    dNiños = cAdultos * 0.30;
+    cN = dNiños * cNiños;
+    cA= cAdultos * cEstandarHospedaje;
+    tnoche=cN + cA;
+    tnoches = tnoche * nHotel;
+    return tnoches; 
+    }
+
+    
+
+
+
+}
