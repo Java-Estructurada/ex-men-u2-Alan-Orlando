d9f2a3f921b32c77ed2a29cb68ce27d3b1eca141
Alan-Orlando <L25290571@cdguzman.tecnm.mx>
2026-03-11 14:31:21 +0000

    examen unidad prueba

--- herramientasCalculos.java
+++ herramientasCalculos.java
@@ -11,17 +11,37 @@ public class herramientasCalculos {
     }
 
     public static int CostoHospedaje (int cNiños,int cAdultos, int cEstandarHospedaje ){
-    int cN,cA;
+    int cNi,cAd;
     dNiños = cAdultos * 0.30;
-    cN = dNiños * cNiños;
-    cA= cAdultos * cEstandarHospedaje;
-    tnoche=cN + cA;
+    cNi = dNiños * cNiños;
+    cAd= cAdultos * cEstandarHospedaje;
+    tnoche=cNi + cAd;
     tnoches = tnoche * nHotel;
     return tnoches; 
     }
 
+    public static int CostoTotal (int cNiños,int cAdultos, int cEstandarVuelo, int cEstandarHospedaje ){
+    int cN,cA,cNi,cAd;
+    dNiños = cAdultos / 2;
+    cN = dNiños * cNiños;
+    cA= cAdultos * cEstandarVuelo;
+    tViaje=cN + cA;
+
+    dNiños = cAdultos * 0.30;
+    cNi = dNiños * cNiños;
+    cAd= cAdultos * cEstandarHospedaje;
+    tnoche=cNi + cAd;
+    tnoches = tnoche * nHotel;
+
+    Total = tnoches + tViaje;
     
 
 
+    
+    }
+
 
 }
+/*Subtotal del Viaje:
+
+La suma exacta del costo total de vuelos y el costo total de hospedaje. */
